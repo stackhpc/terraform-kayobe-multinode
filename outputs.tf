2fb0d12a8ba45ff29833d9fc0653fb497b584de0
Grzegorz Koper <grzegorzkoper@stackhpc.com>
2022-08-02 14:22:03 +0200

    adding CephOSDs output

--- outputs.tf
+++ outputs.tf
@@ -14,10 +14,14 @@ output "access_interface" {
   value = "eth0"
 }
 
-output "controllers_ips" {
+output "controller_ips" {
   value = join("\n", formatlist("%s # %s", openstack_compute_instance_v2.controller.*.name, openstack_compute_instance_v2.controller.*.access_ip_v4))
 }
 
 output "compute_ips" {
   value = join("\n", formatlist("%s # %s", openstack_compute_instance_v2.compute.*.name, openstack_compute_instance_v2.compute.*.access_ip_v4))
+}
+
+output "CephOSD_ips" {
+  value = join("\n", formatlist("%s # %s", openstack_compute_instance_v2.Ceph-OSD.*.name, openstack_compute_instance_v2.Ceph-OSD.*.access_ip_v4))
 }

a20a032f5631dc497ea0d87ccd117dfc25bb1024
Grzegorz Koper <grzegorzkoper@stackhpc.com>
2022-08-02 16:16:54 +0200

    outputing ansible_inventory file

--- outputs.tf
+++ outputs.tf
@@ -25,3 +25,22 @@ output "compute_ips" {
 output "CephOSD_ips" {
   value = join("\n", formatlist("%s # %s", openstack_compute_instance_v2.Ceph-OSD.*.name, openstack_compute_instance_v2.Ceph-OSD.*.access_ip_v4))
 }
+
+resource "local_file" "inventory" {
+  content = templatefile(
+    "${path.module}/templates/ansible_inventory.tpl",
+    { 
+      user = "centos"
+      prefix = "kayobe"
+      compute_hostname = openstack_compute_instance_v2.compute.*.name
+      controller_hostname = openstack_compute_instance_v2.controller.*.name
+      seed_hostname = openstack_compute_instance_v2.kayobe-seed.name
+      seed = openstack_compute_instance_v2.kayobe-seed.access_ip_v4
+      computes = openstack_compute_instance_v2.compute.*.access_ip_v4
+      controllers = openstack_compute_instance_v2.controller.*.access_ip_v4
+      cephOSD_hostname = openstack_compute_instance_v2.Ceph-OSD.*.name
+      cephOSDs =  openstack_compute_instance_v2.Ceph-OSD.*.access_ip_v4
+    }
+  )
+  filename = "ansible_inventory"
+}

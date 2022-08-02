1d0216e11a5c23812ef6c5411448e1b4ae52b083
Grzegorz Koper <grzegorzkoper@stackhpc.com>
2022-08-02 16:26:06 +0200

    fixing tokens to match template

--- outputs.tf
+++ outputs.tf
@@ -36,7 +36,7 @@ resource "local_file" "inventory" {
       controller_hostname = openstack_compute_instance_v2.controller.*.name
       seed_hostname = openstack_compute_instance_v2.kayobe-seed.name
       seed = openstack_compute_instance_v2.kayobe-seed.access_ip_v4
-      computes = openstack_compute_instance_v2.compute.*.access_ip_v4
+      compute = openstack_compute_instance_v2.compute.*.access_ip_v4
       controllers = openstack_compute_instance_v2.controller.*.access_ip_v4
       cephOSD_hostname = openstack_compute_instance_v2.Ceph-OSD.*.name
       cephOSDs =  openstack_compute_instance_v2.Ceph-OSD.*.access_ip_v4

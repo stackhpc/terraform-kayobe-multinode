a5c15f17940e95a0069525f95e937b6afd50c293
Jack Hodgkiss <identity@jackhodgkiss.uk>
2022-10-31 15:41:53 +0000

    feat: remove unecessary outputs

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,3 @@
-output "ansible_control_access_ip_v4" {
-  value = openstack_compute_instance_v2.ansible_control.access_ip_v4
-}
-
-output "access_cidr" {
-  value = data.openstack_networking_subnet_v2.network.cidr
-}
-
-output "access_gw" {
-  value = data.openstack_networking_subnet_v2.network.gateway_ip
-}
-
-output "controller_ips" {
-  value = join("\n", formatlist("%s:  %s", openstack_compute_instance_v2.controller.*.name, openstack_compute_instance_v2.controller.*.access_ip_v4))
-}
-
-output "compute_ips" {
-  value = join("\n", formatlist("%s: %s", openstack_compute_instance_v2.compute.*.name, openstack_compute_instance_v2.compute.*.access_ip_v4))
-}
-
-output "storage_ips" {
-  value = join("\n", formatlist("%s: %s", openstack_compute_instance_v2.storage.*.name, openstack_compute_instance_v2.storage.*.access_ip_v4))
-}
-
 resource "local_file" "hosts" {
   content = templatefile(
     "${path.module}/templates/hosts.tpl",

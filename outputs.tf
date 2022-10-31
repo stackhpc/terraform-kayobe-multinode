20f328d24f7f2bf5a70d7ac298a4059a098cd92e
Jack Hodgkiss <identity@jackhodgkiss.uk>
2022-10-31 15:50:22 +0000

    feat: add ansible ip back to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "ansible_control_access_ip_v4" {
+  value = openstack_compute_instance_v2.ansible_control.access_ip_v4
+}
+
 resource "local_file" "hosts" {
   content = templatefile(
     "${path.module}/templates/hosts.tpl",

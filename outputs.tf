a75a201b504b9fb835836814359abe8280ecc8df
Max.Bed4d <max.bed4d@googlemail.com>
2024-01-08 12:10:54 +0000

    Alter cluster_nodes variables.

--- outputs.tf
+++ outputs.tf
@@ -57,8 +57,8 @@ output "cluster_nodes" {
   value = concat( 
     [
       {
-        name          = "${openstack_compute_instance_v2.compute.*.name}"
-        ip            = "${openstack_compute_instance_v2.compute.*.access_ip_v4}"
+        name          = "${compute_hostname}"
+        ip            = "${compute}"
         groups        = ["compute"]
       }
     ]

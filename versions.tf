a40845e63c5663f27777e7aabe42609176b7b316
Max.Bed4d <max.bed4d@googlemail.com>
2024-01-10 12:08:34 +0000

    This is a combination of 5 commits.

--- versions.tf
+++ versions.tf
@@ -5,9 +5,9 @@ terraform {
       source  = "terraform-provider-openstack/openstack"
       version = "1.49.0"
     }
-    ansible = {
-      source = "ansible/ansible"
-      version = "1.1.0"
-    }
+    #ansible = {
+    #  source = "ansible/ansible"
+    #  version = "1.1.0"
+    #}
   }
 }

2a560e03665164efa24ec52849aad6c57c69c956
Rowan Johns <rowan@stackhpc.com>
2026-09-24 14:47:48 +0100

    add sct functionality

--- ansible/files/multinode.sh
+++ ansible/files/multinode.sh
@@ -378,64 +378,34 @@ function run_tempest() {
 }
 
 function run_sct() {
-# Run SCT. Return non-zero if any tests failed.
-
-  sct_dir="$HOME/sct-results"
-
+  # Run SCT on the seed. Return non-zero if any tests failed.
+  cd
   seed_ssh=$(get_seed_ssh)
 
-  git -C ${config_directories[kayobe]} submodule init
-  git -C ${config_directories[kayobe]} submodule update
-
-  build_kayobe_image
-
-  set +x
-  export KAYOBE_AUTOMATION_SSH_PRIVATE_KEY=$(cat ~/.ssh/id_rsa)
-  set -x
-
-  if [[ -d $sct_dir ]]; then
-    sct_backup=${sct_dir}-$(date +%Y%m%dT%H%M%S)
-    echo "Found previous SCT results"
-    echo "Moving to $sct_backup"
-    mv $sct_dir $sct_backup
-  fi
-
-  # Remove any previous kayobe_sct container
-  sudo docker rm kayobe_sct || true
-
-  sudo -E docker run -t \
-    --name kayobe_sct \
-    -v ${config_directories[kayobe]}:/stack/kayobe-automation-env/src/kayobe-config \
-    -v $sct_dir:/stack/sct-results \
-    -e KAYOBE_ENVIRONMENT -e KAYOBE_VAULT_PASSWORD -e KAYOBE_AUTOMATION_SSH_PRIVATE_KEY \
-    $KAYOBE_IMAGE \
-    /stack/kayobe-automation-env/src/kayobe-config/.automation/pipeline/playbook-run.sh '$KAYOBE_CONFIG_PATH/ansible/tools/stackhpc-cloud-tests.yml' \
-    -e sct_version=0.2.0
-
-  if ! ssh -oStrictHostKeyChecking=no ${seed_ssh} 'sudo docker logs --follow sct'; then
-    echo "Failed to follow SCT container logs"
-    echo "Ignoring - this may or may not indicate an error"
-  fi
-
-  # Wait for Kayobe SCT pipeline to complete to ensure artifacts exist.
-  kayobe_sct_rc="$(sudo docker container wait kayobe_sct)"
-  if [[ $kayobe_sct_rc != "0" ]]; then
-    echo "Failed running kayobe_sct container. Output:"
-    sudo docker logs kayobe_sct
-  fi
-  sudo docker rm kayobe_sct
-
-  if [[ ! -f $sct_dir/failed-tests ]]; then
-    echo "Unable to find SCT results in $sct_dir/failed-tests"
+  # TODO: edit file paths to point to automation
+  # Copy kayobe config, SCT script, venvs and ssh keys to the seed.
+  scp -r ${config_directories[kayobe]}/ $seed_ssh:~/
+  scp $HOME/stackhpc-cloud-tests.sh $seed_ssh:~/
+  scp $HOME/setup-cloud-tests.sh $seed_ssh:~/
+  scp $HOME/.ssh/id_rsa* $seed_ssh:~/.ssh/
+  scp $HOME/vault.password $seed_ssh:~/
+  # Set file permissions
+  ssh $seed_ssh sudo chmod +x /home/stack/stackhpc-cloud-tests.sh /home/stack/setup-cloud-tests.sh
+  # Run SCT on the seed.
+  if ! ssh $seed_ssh /home/stack/setup-cloud-tests.sh; then
     return 1
   fi
 
-  if [[ $(wc -l < $sct_dir/failed-tests) -ne 0 ]]; then
-    echo "Some SCT tests failed"
+  # Clean up files that were copied over
+  if ! ssh $seed_ssh rm -rf /home/stack/kayobe-config/; then
+    echo "Kayobe config files could not be cleaned up!"
+    return 1
+  fi
+  if ! ssh $seed_ssh rm stackhpc-cloud-tests.sh setup-cloud-tests.sh; then
+    echo "SCT scripts could not be cleaned up!"
     return 1
   fi
 
-  echo "SCT testing successful"
 }
 
 function run_tests() {
@@ -519,6 +489,7 @@ function usage() {
   echo "  create_resources"
   echo "  build_kayobe_image"
   echo "  run_tempest"
+  echo "  run_sct"
   echo "  upgrade_overcloud"
   echo "  upgrade_prerequisites"
   echo "  minor_upgrade"
@@ -544,7 +515,7 @@ function main() {
       $cmd
       ;;
     # Standard commands.
-    (build_kayobe_image|deploy_full|deploy_seed|deploy_overcloud|deploy_wazuh|create_resources|run_tempest|upgrade_overcloud|upgrade_prerequisites|minor_upgrade)
+    (build_kayobe_image|deploy_full|deploy_seed|deploy_overcloud|deploy_wazuh|create_resources|run_tempest|run_sct|upgrade_overcloud|upgrade_prerequisites|minor_upgrade)
       setup
       $cmd
       report_success
